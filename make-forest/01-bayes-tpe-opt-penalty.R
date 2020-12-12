5615a87d3ed5d04936b626f2a5402d49d05e2d19
Alexander Moran <amoran6@ND.EDU>
2020-12-12 16:37:09 -0500

    Make Bayes forest parallel in 01

--- make-forest/01-bayes-tpe-opt-penalty.R
+++ make-forest/01-bayes-tpe-opt-penalty.R
@@ -609,8 +609,33 @@ bayesian_sprout_tree <- function(formula, feature_frac, sample_data = TRUE, mins
 bayes_reg_rf <- function(formula, n_trees = 50, feature_frac = 0.7, sample_data = TRUE, minsize = NULL, data, penalties = NULL) {
   # apply the rf_tree function n_trees times with plyr::raply
   # - track the progress with a progress bar
-  trees <- plyr::raply(
-    n_trees,
+  split <- detectCores()/2 
+  cl <- makeCluster(split)
+  registerDoParallel(cl)
+  x <- c("dplyr", "tictoc", "ggplot2")
+  clusterExport(cl, c("x", "formula", "n_trees", "feature_frac", "sample_data", 
+                      "minsize", "data", "penalties", "bayesian_sprout_tree", 
+                      "evaluate_penalties", "generate_custom_random", "get_distribution",
+                      "split_lg", "get_rmses", "reg_tree", "sse_var"))
+  init <- clusterEvalQ(cl, lapply(x, require, character.only = TRUE))
+  
+  # trees <- parLapply(
+  # cl,
+  #  rep(1, split),
+  #   bayesian_sprout_tree(
+  #     formula = formula,
+  #     feature_frac = feature_frac,
+  #     sample_data = sample_data,
+  #     minsize = minsize,
+  #     data = data,
+  #     penalties = penalties
+  #   )
+  # )
+  
+  trees <- foreach(
+    rep(1, split),
+    .combine = list,
+    .multicombine = TRUE) %dopar%
     bayesian_sprout_tree(
       formula = formula,
       feature_frac = feature_frac,
@@ -618,9 +643,10 @@ bayes_reg_rf <- function(formula, n_trees = 50, feature_frac = 0.7, sample_data 
       minsize = minsize,
       data = data,
       penalties = penalties
-    ),
-    .progress = "text"
-  )
+    )
+
+  
+  stopCluster(cl)
   
   return(trees)
 }
