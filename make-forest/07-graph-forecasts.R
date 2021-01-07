f6dd4f4fb89b81e7da9d3e842356be20ee77ad7a
Alex Moran <alexander.moran99@gmail.com>
2021-01-07 14:37:42 -0500

    Run and modify script 07

--- make-forest/07-graph-forecasts.R
+++ make-forest/07-graph-forecasts.R
@@ -4,13 +4,13 @@ header <- source("header.R")
 
 #Code ------------------------------------------
 df <- read_rds(paste0(export, "master_data.rds"))
-ar1_pred_forecast <- read_rds(paste0(export, "4_year_forecasts/ar1_forecast.rds"))
 arima_forecast <- read_rds(paste0(export, "4_year_forecasts/arima_forecast.rds"))
 ar1_fat_leaf_forecast <- read_rds(paste0(export, "4_year_forecasts/ar1_obj_forecast.rds"))
 ar1_fat_leaf_sample <- read_rds(paste0(export, "4_year_forecasts/ar1_obj_forecast_sample.rds"))
 arima_forecast <- ts(arima_forecast, start = c(1999, 1), frequency = 12)
 ar1_ts <- ts(ar1_fat_leaf_forecast, start = c(1999, 1), frequency = 12)
 ar1_sample_ts <- ts(ar1_fat_leaf_sample, start = c(1999,1), frequency = 12)
+ar1_mean <- (ar1_ts + ar1_sample_ts)/2
 
 values_df <- df %>% 
   dplyr::filter(year >= 1959)
@@ -25,15 +25,21 @@ graph_df <- data.frame(date = seq(as.Date("1999/1/1"), as.Date("2020/1/1"), "mon
   dplyr::mutate(arima = arima_forecast,
                 ar1 = ar1_fat_leaf_forecast,
                 sample = ar1_fat_leaf_sample,
+                mean = ar1_mean,
                 infl = infl)
 
 graph_df$ar1_diff <- graph_df$ar1 - graph_df$infl
 graph_df$arima_diff <- graph_df$arima - graph_df$infl
 graph_df$ar1_sample_diff <- graph_df$sample - graph_df$infl
+graph_df$ar1_mean_diff <- ar1_mean - graph_df$infl
 graph_df$closer <- ifelse(abs(graph_df$ar1_diff) < abs(graph_df$arima_diff), "ar1", "arima")
 graph_df$closer <- as.factor(graph_df$closer)
 graph_df$sample_closer <- ifelse(abs(graph_df$ar1_sample_diff) < abs(graph_df$arima_diff), "ar1", "arima")
 graph_df$sample_closer <- as.factor(graph_df$sample_closer)
+graph_df$mean_closer <- ifelse(abs(graph_df$ar1_mean_diff) < abs(graph_df$arima_diff), "ar1", "arima")
+graph_df$mean_closer <- as.factor(graph_df$mean_closer)
+graph_df$any_closer <- ifelse(graph_df$closer == "ar1" | graph_df$sample_closer == "ar1", "ar1", "arima")
+graph_df$any_closer <- as.factor(graph_df$any_closer)
 
 tidy_graph <- gather(data = graph_df, key = "key", value = "value", "arima":"infl")
 
@@ -44,6 +50,8 @@ plot
 # Compare sample vs regular and ARIMA ---------------
 count(graph_df, closer)
 count(graph_df, sample_closer)
+count(graph_df, mean_closer)
+count(graph_df, any_closer)
 
 accuracy(tsData, ar1_ts)
 accuracy(tsData, ar1_sample_ts)
@@ -51,5 +59,6 @@ accuracy(tsData, arima_forecast)
 accuracy(tsData, naive)
 
 mean(infl)
-mean(ar1_fat_leaf_forecast)
+mean(ar1_ts)
+mean(ar1_sample_ts)
 mean(arima_forecast)
