d30ea513e748008b8e8a2873aeb25bea1370a01c
Alex Moran <alexander.moran99@gmail.com>
2021-01-27 14:30:22 -0500

    Explore mean vs improved forest

--- make-forest/07-graph-forecasts.R
+++ make-forest/07-graph-forecasts.R
@@ -12,6 +12,7 @@ arima_forecast <- ts(arima_forecast, start = c(1999, 1), frequency = 12)
 ar1_ts <- ts(ar1_fat_leaf_forecast, start = c(1999, 1), frequency = 12)
 ar1_sample_ts <- ts(ar1_fat_leaf_sample, start = c(1999,1), frequency = 12)
 ar1_mean <- (ar1_ts + ar1_sample_ts)/2
+mean <- read_rds(paste0(export, "4_year_forecasts/mean_forecast_sample.rds"))
 
 values_df <- df %>% 
   dplyr::filter(year >= 1959)
@@ -26,7 +27,8 @@ graph_df <- data.frame(date = seq(as.Date("1999/1/1"), as.Date("2020/1/1"), "mon
   dplyr::mutate(arima = arima_forecast,
                 ar1 = ar1_fat_leaf_forecast,
                 sample = ar1_fat_leaf_sample,
-                mean = ar1_mean,
+                ar1_mean = ar1_mean,
+                mean = mean,
                 strict_ar1 = strict_ar1,
                 naive = naive,
                 infl = infl)
@@ -36,16 +38,19 @@ graph_df$arima_diff <- graph_df$arima - graph_df$infl
 graph_df$ar1_sample_diff <- graph_df$sample - graph_df$infl
 graph_df$ar1_mean_diff <- ar1_mean - graph_df$infl
 graph_df$strict_diff <- graph_df$strict_ar1 - graph_df$infl
+graph_df$mean_diff <- graph_df$mean - graph_df$infl
 graph_df$closer <- ifelse(abs(graph_df$ar1_diff) < abs(graph_df$arima_diff), "ar1", "arima")
 graph_df$closer <- as.factor(graph_df$closer)
 graph_df$sample_closer <- ifelse(abs(graph_df$ar1_sample_diff) < abs(graph_df$arima_diff), "ar1", "arima")
 graph_df$sample_closer <- as.factor(graph_df$sample_closer)
-graph_df$mean_closer <- ifelse(abs(graph_df$ar1_mean_diff) < abs(graph_df$arima_diff), "ar1", "arima")
-graph_df$mean_closer <- as.factor(graph_df$mean_closer)
+graph_df$ar1_mean_closer <- ifelse(abs(graph_df$ar1_mean_diff) < abs(graph_df$arima_diff), "ar1", "arima")
+graph_df$ar1_mean_closer <- as.factor(graph_df$ar1_mean_closer)
 graph_df$strict_closer <- ifelse(abs(graph_df$ar1_sample_diff) < abs(graph_df$strict_diff), "ar1", "arima")
 graph_df$strict_closer <- as.factor(graph_df$strict_closer)
 graph_df$any_closer <- ifelse(graph_df$closer == "ar1" | graph_df$sample_closer == "ar1", "ar1", "arima")
 graph_df$any_closer <- as.factor(graph_df$any_closer)
+graph_df$mean_closer <- ifelse(abs(graph_df$mean_diff) < abs(graph_df$arima_diff), "mean", "arima")
+graph_df$mean_closer <- as.factor(graph_df$mean_closer)
 
 tidy_graph <- gather(data = graph_df, key = "key", value = "value", "arima":"infl")
 
@@ -55,12 +60,14 @@ plot
 
 # Compare sample vs regular and ARIMA ---------------
 count(graph_df, closer)
-count(graph_df, sample_closer)
 count(graph_df, mean_closer)
+count(graph_df, sample_closer)
+count(graph_df, ar1_mean_closer)
 count(graph_df, any_closer)
 count(graph_df, strict_closer)
 
 accuracy(tsData, ar1_ts)
+accuracy(tsData, mean)
 accuracy(tsData, ar1_sample_ts)
 accuracy(tsData, ar1_mean)
 accuracy(tsData, arima_forecast)
@@ -75,12 +82,12 @@ mean(arima_forecast)
 
 #Compare mean of forests to ARIMA ------------------------------
 mean_better <- graph_df %>% 
-  filter(mean_closer == "ar1") %>% 
-  select(date, arima, mean, infl, arima_diff, ar1_mean_diff)
+  filter(ar1_mean_closer == "ar1") %>% 
+  select(date, arima, ar1_mean, infl, arima_diff, ar1_mean_diff)
 
 arima_better <- graph_df %>% 
-  filter(mean_closer == "arima") %>% 
-  select(date, arima, mean, infl, arima_diff, ar1_mean_diff)
+  filter(ar1_mean_closer == "arima") %>% 
+  select(date, arima, ar1_mean, infl, arima_diff, ar1_mean_diff)
 
 mean_tidy <- gather(mean_better, key = "key", value = "value", "arima_diff":"ar1_mean_diff")
 arima_tidy <- gather(arima_better, key = "key", value = "value", "arima_diff":"ar1_mean_diff")
--- make-forest/10-just-for-fun.R
+++ make-forest/10-just-for-fun.R
@@ -5,4 +5,17 @@ header <- source("header.R")
 #Code ------------------------------------------
 mentions <- read_rds(paste0(export, "custom_forest_analysis/mentions.rds"))
 totals <- data.frame(name = rownames(mentions), total = mentions$total)
+totals$per_forest <- totals$total/253
+totals$per_tree <- totals$per_forest/50
+totals$per_real_tree <- totals$per_tree/totals[13,4]
 barplot(H = totals$total, xlab = totals$name, height = totals$total)
+
+mean_mentions <- read_rds(paste0(export, "custom_forest_analysis/mean_forest_mentions.rds"))
+mean_totals <- data.frame(name = rownames(mean_mentions), total = mean_mentions$total)
+mean_totals$per_forest <- mean_totals$total/253
+mean_totals$per_tree <- mean_totals$per_forest/50
+mean_totals$per_real_tree <- mean_totals$per_tree/mean_totals[13,4]
+barplot(H = mean_totals$toal, xlab = mean_totals$name, height = mean_totals$total)
+
+diff_df <- data.frame(name = rownames(mentions), mean_tree = mean_totals$per_real_tree, tree = totals$per_real_tree)
+diff_df$diff <- diff_df$tree/diff_df$mean_tree
