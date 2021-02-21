8a0bca65bbf668967bc9bb843bc7297864bb94e9
Alex Moran <alexander.moran99@gmail.com>
2021-02-20 23:56:23 -0500

    Change UK infl to start in 2015

--- other-cases/03b-uk-inflation-graph.R
+++ other-cases/03b-uk-inflation-graph.R
@@ -22,14 +22,14 @@ values_df <- values_df[-1,]
 rownames(values_df) <- seq(1, nrow(values_df))
 tsData <- ts(values_df$infl, start = c(1988, 2), frequency = 12)
 
-infl <- window(tsData, start = c(2010, 1), end = c(2020, 1))
+infl <- window(tsData, start = c(2015, 1), end = c(2020, 1))
 
 naive_forecast <- window(tsData, start = c(2009, 12), end = c(2019, 12))
 naive <- ts(naive_forecast, start = c(2010, 1), frequency = 12)
 
-graph_df <- data.frame(date = seq(as.Date("2010/1/1"), as.Date("2020/1/1"), "month")) %>% 
-  dplyr::mutate(arima = arima_forecast,
-                forest = forest_forecast,
+graph_df <- data.frame(date = seq(as.Date("2015/1/1"), as.Date("2020/1/1"), "month")) %>% 
+  dplyr::mutate(arima = arima_forecast[-c(1:60)],
+                forest = forest_forecast[-c(1:60)],
                 infl = infl)
 
 
@@ -44,3 +44,9 @@ accuracy(tsData, forest_forecast)
 accuracy(tsData, ar1_forecast)
 accuracy(tsData, base_forecast)
 accuracy(tsData, naive)
+
+accuracy(infl, arima_forecast)
+accuracy(infl, forest_forecast)
+accuracy(infl, ar1_forecast)
+accuracy(infl, base_forecast)
+accuracy(infl, naive)
