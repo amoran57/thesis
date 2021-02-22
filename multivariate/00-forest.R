d03871adc24f0b2d0a6450753e18c2564162657b
Alexander Moran <amoran6@ND.EDU>
2021-02-22 16:36:56 -0500

    REady to run multi 00

--- multivariate/00-forest.R
+++ multivariate/00-forest.R
@@ -763,5 +763,3 @@ forest_forecast_ts <- ts(forecasts_rf, start = c(1999, 1), frequency = 12)
 accuracy(tsData, forest_forecast_ts)
 
 write_rds(forest_forecast_ts, paste0(export,"multivariate/forecast.rds"))
-
-#R2R
