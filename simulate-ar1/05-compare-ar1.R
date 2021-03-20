255f92f79f0c7b77b75f31266040109699ce1de9
Alex Moran <alexander.moran99@gmail.com>
2021-03-20 18:50:15 -0400

    Add Appendix 4, make tables relative

--- simulate-ar1/05-compare-ar1.R
+++ simulate-ar1/05-compare-ar1.R
@@ -15,10 +15,12 @@ obj_ts <- ts(obj_fit)
 pred_ts <- ts(pred_fit)
 mean_ts <- ts(mean_fit)
 y_ts <- ts(yt[-c(1:11)])
+naive_ts <- ts(yt[-c(1:10, 501)])
 
 #Compare --------------------------------------
 graph_df <- data.frame(index = seq(1, length(obj_fit)), 
                        simulated = yt[-c(1:11)], 
+                       naive = naive_ts,
                        arima = arima_fit[-c(1:11)], 
                        obj = obj_fit,
                        pred = pred_fit,
@@ -32,28 +34,31 @@ accuracy(y_ts, arima_ts)
 accuracy(y_ts, obj_ts)
 accuracy(y_ts, pred_ts)
 accuracy(y_ts, mean_ts)
+accuracy(y_ts, naive_ts)
 
 # Again, with forecasts --------------------------------
 #Import -------------------------------
 yt <- read_rds(paste0(simulate_ar1, "ar1-data/simulated-data.rds"))
-obj_fit <- read_rds(paste0(simulate_ar1, "ar1-data/obj-fit-forecast.rds"))
-pred_fit <- read_rds(paste0(simulate_ar1, "ar1-data/pred-fit-forecast.rds"))
-mean_fit <- read_rds(paste0(simulate_ar1, "ar1-data/mean-fit-forecast.rds"))
-arima_fit <- read_rds(paste0(simulate_ar1, "ar1-data/arima-forecast.rds"))
+obj_forecast <- read_rds(paste0(simulate_ar1, "ar1-data/obj-fit-forecast.rds"))
+pred_forecast <- read_rds(paste0(simulate_ar1, "ar1-data/pred-fit-forecast.rds"))
+mean_forecast <- read_rds(paste0(simulate_ar1, "ar1-data/mean-fit-forecast.rds"))
+arima_forecast <- read_rds(paste0(simulate_ar1, "ar1-data/arima-forecast.rds"))
 
-arima_ts <- ts(arima_fit)
-obj_ts <- ts(obj_fit)
-pred_ts <- ts(pred_fit)
-mean_ts <- ts(mean_fit)
+arima_ts <- ts(arima_forecast)
+obj_ts <- ts(obj_forecast)
+pred_ts <- ts(pred_forecast)
+mean_ts <- ts(mean_forecast)
+naive_ts <- ts(yt[c(410:510)])
 y_ts <- ts(yt[c(411:511)])
 
 #Compare --------------------------------------
-graph_df <- data.frame(index = seq(1, length(obj_fit)), 
+graph_df <- data.frame(index = seq(1, length(obj_forecast)), 
                        simulated = yt[c(411:511)], 
-                       arima = arima_fit, 
-                       pred = pred_fit,
-                       mean = mean_fit,
-                       obj = obj_fit)
+                       naive = naive_ts,
+                       arima = arima_forecast, 
+                       pred = pred_forecast,
+                       mean = mean_forecast,
+                       obj = obj_forecast)
 tidy_graph_df <- gather(data = graph_df, key = "key", value = "value", "simulated":"obj")
 plot <- ggplot(data = tidy_graph_df, aes(x = index, y = value, color = key)) +
   geom_line()
@@ -63,5 +68,5 @@ accuracy(y_ts, arima_ts)
 accuracy(y_ts, obj_ts)
 accuracy(y_ts, pred_ts)
 accuracy(y_ts, mean_ts)
-
+accuracy(y_ts, naive_ts)
 
